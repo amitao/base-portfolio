b979d390a42e1f08ed919547ad02d100ec0a2c2f
Amie Thao <amie.c.thao@gmail.com>
2018-12-15 10:25:14 -0600

    fix error in index.js

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+// stateless component
+const Header = () => {
+  return <div>Hi three!</div>
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Components/App/App.js';
 import * as serviceWorker from './serviceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+// Provider allows us to use redux within our react app
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+// Import saga middleware
+import createSagaMiddleware from 'redux-saga';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create the rootSaga generator function
+function* rootSaga() {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+}
+
+// Create sagaMiddleware
+const sagaMiddleware = createSagaMiddleware();
+
+// Used to store projects returned from the server
+const projects = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_PROJECTS':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+// Used to store the project tags (e.g. 'React', 'jQuery', 'Angular', 'Node.js')
+const tags = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_TAGS':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+// Create one store that all components can use
+const storeInstance = createStore(
+    combineReducers({
+        projects,
+        tags,
+    }),
+    // Add sagaMiddleware to our store
+    applyMiddleware(sagaMiddleware, logger),
+);
+
+// Pass rootSaga into our sagaMiddleware
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, 
+    document.getElementById('root'));
 serviceWorker.unregister();
